b552c99e3a16c29f9894fd1d0886b3a5d3dadfed
Sumeya <sumeyabashraheel@gmail.com>
2024-08-23 08:57:54 +0100

    experiment

--- src/components/Tasks/TasksUI/DeleteTaskBtn.jsx
+++
@@ -1,8 +0,0 @@
-import { TasksContext } from "../../../Contexts/ComponentContexts";
-import { useContext } from "react";
-
-export default function DeleteTasksBtn({ taskId }) {
-  const { deleteTask } = useContext(TasksContext);
-
-  return <button onClick={() => deleteTask(taskId)}>x</button>;
-}
